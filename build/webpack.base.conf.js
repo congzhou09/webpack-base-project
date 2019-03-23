d904eeaf4b64a4ef85a3284684655c183f049b8c
congzhou09 <yuancongzhou@yeah.net>
2019-03-23 14:45:24 +0800

    dist的js文件夹

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -12,7 +12,7 @@ module.exports = {
         app: './src/main.js'
     },
     output: {
-        path: resolve("dist"),
+        path: resolve("dist/js"),
         filename: "[name]-[hash].js"
     },
     module: {
