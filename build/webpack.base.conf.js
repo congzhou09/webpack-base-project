01ff9ff3b683bcfa30585e25fda91841517a2a3d
congzhou09 <yuancongzhou@yeah.net>
2019-03-23 12:26:10 +0800

    1.babel@7的babelrc新写法 2.webpack@4的loader的option新写法

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -28,19 +28,23 @@ module.exports = {
             },
             {
                 test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-                use: 'url-loader',
-                options: {
-                    limit: 10000,
-                    name: 'static/img/[name].[hash:7].[ext]'
-                }
+                use:[{
+                    loader: 'url-loader',
+                    options: {
+                        limit: 10000,
+                        name: 'static/img/[name].[hash:7].[ext]'
+                    }
+                }]
             },
             {
                 test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-                use: 'url-loader',
-                options: {
-                    limit: 10000,
-                    name: 'static/fonts/[name].[hash:7].[ext]'
-                }
+                use: [{
+                    loader: 'url-loader',
+                    options: {
+                        limit: 10000,
+                        name: 'static/fonts/[name].[hash:7].[ext]'
+                    }
+                }]
             }
         ]
     },
