f6de61981a1969dfbc9f103ae42cf015028e44b8
congzhou09 <yuancongzhou@yeah.net>
2019-03-23 11:52:50 +0800

    图片、字体、css

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -21,6 +21,26 @@ module.exports = {
                 test: /\.js$/,
                 include: [resolve('src')],
                 use: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader','css-loader']
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+                use: 'url-loader',
+                options: {
+                    limit: 10000,
+                    name: 'static/img/[name].[hash:7].[ext]'
+                }
+            },
+            {
+                test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+                use: 'url-loader',
+                options: {
+                    limit: 10000,
+                    name: 'static/fonts/[name].[hash:7].[ext]'
+                }
             }
         ]
     },
