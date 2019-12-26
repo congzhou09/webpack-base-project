b91cdf54f8978fc33dbcb2a8f40723942d9a98d3
congzhou09 <yuancongzhou@yeah.net>
2019-12-26 10:01:45 +0800

    chore: 代码格式化补充

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,13 @@
 module.exports = {
-    presets: [
-        [
-            "@babel/preset-env",
-            {
-                "targets": {
-                    "browsers": ["> 1%", "last 2 versions", "not ie <= 8"]
-                }
-            }
-        ],
-        "@babel/preset-react"
-    ]
-}
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          browsers: ['> 1%', 'last 2 versions', 'not ie <= 8']
+        }
+      }
+    ],
+    '@babel/preset-react'
+  ]
+};
