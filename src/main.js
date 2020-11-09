cdd4de66b900726eb6a2e79a572e1bee95ebd4aa
congzhou09 <yuancongzhou@yeah.net>
2020-11-09 14:37:07 +0800

    增加prop-types依赖

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import './style/all.css';
 import React from 'react';
 import ReactDom from 'react-dom';
+import PropTypes from 'prop-types';
 
-ReactDom.render(
-  <div>
-    <p>Hello World!</p>
-  </div>,
-  document.querySelector('#app')
-);
+const App = props => {
+  return <p>{props.message}</p>;
+};
+
+App.propTypes = {
+  message: PropTypes.string
+};
+
+ReactDom.render(<App message="Hello" />, document.querySelector('#app'));
