0a2311bbdf79063656eecf66384f2811629a8aea
congzhou09 <yuancongzhou@yeah.net>
2022-10-25 10:55:43 +0800

    vite use root vite.html

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig((command, mode, ssrBuild) => {
+  return {
+    // root: 'vite',
+    build: {
+      rollupOptions: {
+        input: {
+          app: './vite.html',
+        },
+      },
+    },
+    server: {
+      open: './vite.html',
+    },
+  };
+});
