7efd1bc0f3c138c9e5eb0ff7d0f8ca75a78cb4bd
congzhou09 <yuancongzhou@yeah.net>
2022-11-01 17:41:43 +0800

    feat:babel与codecheck完善

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,17 @@
 module.exports = {
+  plugins: [
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
+  ],
   presets: [
     [
       '@babel/preset-env',
       {
         targets: {
-          browsers: ['> 1%', 'last 2 versions', 'not ie <= 8']
-        }
-      }
+          browsers: ['> 1%', 'last 2 versions', 'not ie <= 8'],
+        },
+      },
     ],
-    '@babel/preset-react'
-  ]
+    '@babel/preset-react',
+  ],
 };
