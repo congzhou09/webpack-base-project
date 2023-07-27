f9e0d4f6b92abaea19a4b42772ffc110ae38f0c3
congzhou09 <yuancongzhou@yeah.net>
2023-07-27 19:45:43 +0800

    feat:merge script revision

--- scripts/merge.js
+++ scripts/merge.js
@@ -1,17 +1,20 @@
 const { execSync } = require('child_process');
-// const shell = require('shelljs'); // 执行文件操作
 
 function mergeOne(oneBranch) {
   execSync(`git checkout ${oneBranch}`);
   execSync('git pull');
   execSync(`git merge master`);
-  execSync(`git checkout master`);
+  execSync(`git push`);
+  // execSync(`git checkout master`);
 }
 
 function run() {
-  // execSync('git fetch origin master:master');
-  ['react'].forEach((oneBranch) => {
-    mergeOne(oneBranch);
+  const args = process.argv.slice(2);
+  const validBranch = ['react', 'vue'];
+  args.forEach((oneBranch) => {
+    if (validBranch.includes(oneBranch)) {
+      mergeOne(oneBranch);
+    }
   });
 }
 
