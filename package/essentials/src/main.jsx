933f7706b8ad203e35e729950ac58ceadd307578
congzhou09 <yuancongzhou@yeah.net>
2023-09-06 17:22:36 +0800

    feat:package new version

--- package/essentials/src/main.jsx
+++ package/essentials/src/main.jsx
@@ -6,8 +6,30 @@ import '@utils/one';
 
 const root = createRoot(document.querySelector('#app'));
 
+class Counter extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 1,
+    };
+  }
+  onAdd() {
+    this.setState((prevState) => ({
+      count: prevState.count + 1,
+    }));
+  }
+  render() {
+    return <button onClick={() => this.onAdd()}>{this.state.count}</button>;
+  }
+}
+
 const App = (props) => {
-  return <p>{props.message}</p>;
+  return (
+    <div>
+      <p>{props.message}</p>
+      <Counter />
+    </div>
+  );
 };
 
 App.propTypes = {
