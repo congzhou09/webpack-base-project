66b2b38307f8de352382359058d44cd72373956c
congzhou09 <yuancongzhou@yeah.net>
2024-01-10 09:30:43 +0800

    chore: package update

--- package/essentials/src/demo.test.tsx
+++ package/essentials/src/demo.test.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Counter } from './demo';
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 
 describe('Demo', () => {
-  it('renders Counter component', () => {
-    render(<Counter />);
-    screen.debug();
+  render(<Counter />);
+  // screen.debug();
+
+  it('initial', () => {
+    const addBtn = screen.queryByTestId('add-btn');
+    expect(addBtn).toBeInTheDocument();
+    expect(addBtn).toHaveTextContent('1');
   });
 });
