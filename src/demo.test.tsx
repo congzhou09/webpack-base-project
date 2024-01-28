28d0ca5266a06e74d550089776de937c94e2a10c
congzhou09 <yuancongzhou@yeah.net>
2024-01-28 23:17:34 +0800

    feat:add jest demo with userEvent

--- src/demo.test.tsx
+++ src/demo.test.tsx
@@ -1,31 +1,58 @@
-import React from 'react';
 import { Counter, NewsBoard } from './demo';
-import { render, screen, waitFor } from '@testing-library/react';
+import { render, screen, act } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event';
 
-describe.skip('test Counter', () => {
-  render(<Counter />);
-  // screen.debug();
+describe('test Counter', () => {
+  beforeEach(() => {
+    render(<Counter />);
+  });
 
   it('initial', () => {
     const addBtn = screen.queryByTestId('add-btn');
     expect(addBtn).toBeInTheDocument();
     expect(addBtn).toHaveTextContent('1');
   });
+
+  it('add-button click', async () => {
+    const addBtn = screen.queryByTestId('add-btn');
+    await userEvent.click(addBtn);
+    expect(addBtn).toHaveTextContent('2');
+  });
 });
 
 describe('test NewsBoard', () => {
-  it('NewsBoard first render', async () => {
-    global.fetch = jest.fn(() => {
-      return Promise.resolve({
-        json: () =>
-          Promise.resolve({
-            hits: [{ title: 'Free React.js Fundamentals Course', url: 'http://courses.reactjsprogram.com/courses/reactjsfundamentals' }],
-          }),
-      });
-    }) as jest.Mock;
-    render(<NewsBoard />);
+  global.fetch = jest.fn((reqUrl) => {
+    const searchStr = reqUrl.substring(reqUrl.indexOf('?query=') + 7);
+    let resData = [{ title: 'my blog', url: 'https://congzhou09.github.io' }];
+    if (searchStr.includes('react.js')) {
+      resData = [{ title: 'Free React.js Fundamentals Course', url: 'http://courses.reactjsprogram.com/courses/reactjsfundamentals' }];
+    }
+    return Promise.resolve({
+      json: () =>
+        Promise.resolve({
+          hits: resData,
+        }),
+    });
+  }) as jest.Mock;
+
+  beforeEach(async () => {
+    await act(async () => {
+      render(<NewsBoard />);
+    });
+  });
+
+  it('first render', async () => {
     expect(await screen.findByText(/Free React.js/)).toBeInTheDocument();
+    screen.debug();
+  });
+
+  it('search-btn click', async () => {
+    const keywordInput = screen.getByTestId('keyword-input');
+    await userEvent.clear(keywordInput);
+    expect(keywordInput).toHaveValue('');
+    await userEvent.click(screen.getByTestId('search-btn'));
+    expect(await screen.findByText(/my blog/)).toBeInTheDocument();
     screen.debug();
   });
 });
